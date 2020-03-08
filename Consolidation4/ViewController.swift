8a3dfee5237f1bd7e8d34f2bdd69332fdbc8872e
Roman <romaha48915@gmailcom>
2020-03-08 02:24:01 +0200

    add heartLabel

--- Consolidation4/ViewController.swift
+++ Consolidation4/ViewController.swift
@@ -10,8 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var questionLabel:UILabel!
+    var answerLabel:UILabel!
+    var heartLabel:UILabel!
+    var heart = 5{
+        didSet{
+            heartLabel.text = "❤️ \(heart)"
+        }
+    }
+    
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        heartLabel = UILabel()
+        heartLabel.text = "❤️\(heart)"
+        heartLabel.textAlignment = .right
+        heartLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(heartLabel)
+        
+        heartLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        heartLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
+        
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
