3337d78025e720dfcaf67d2ddefa8d2c8d7fd7f8
Roman <romaha48915@gmailcom>
2020-03-08 02:52:34 +0200

    added questionLabel

--- Consolidation4/ViewController.swift
+++ Consolidation4/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var questionLabel:UILabel!
-    var answerLabel:UITextField!
+    var answerTextField:UITextField!
     var heartLabel:UILabel!
     var heart = 7{
         didSet{
@@ -23,22 +23,31 @@ class ViewController: UIViewController {
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
+        
+        //MARK::adding hearLable
         heartLabel = UILabel()
         heartLabel.text = "❤️\(heart)"
         heartLabel.textAlignment = .right
         heartLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(heartLabel)
        
+        //MARK::adding quetionView
         let questionView = UIView()
         questionView.translatesAutoresizingMaskIntoConstraints = false
-        questionView.backgroundColor = UIColor.green
-       
         view.addSubview(questionView)
         
+        //MARK::adding questionLabel
+        questionLabel = UILabel()
+        questionLabel.translatesAutoresizingMaskIntoConstraints = false
+        questionLabel.text = "What is you name?"
+        questionLabel.textAlignment = .center
+        questionLabel.font = UIFont.systemFont(ofSize: 21)
+        view.addSubview(questionLabel)
         
         
         
         
+        //MARK::anchors
         NSLayoutConstraint.activate([
             heartLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
             heartLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
@@ -46,8 +55,10 @@ class ViewController: UIViewController {
             questionView.topAnchor.constraint(equalTo: heartLabel.bottomAnchor),
             questionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             questionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            questionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25)
+            questionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25),
                    
+            questionLabel.centerYAnchor.constraint(equalTo: questionView.centerYAnchor),
+            questionLabel.centerXAnchor.constraint(equalTo: questionView.centerXAnchor)
                    
         ])
        
